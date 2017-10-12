5b172cfcfba2e24b61614cf038dbd7e262ec9c55
Brian <brian_treichel4@outlook.com>
2017-10-12 15:45:45 -0400

    Added tests
    added some test methods for the controllers

---
+++ JKBB_CVGS_Tests/Controllers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using JKBB_CVGS;
+using JKBB_CVGS.Controllers;
+
+namespace JKBB_CVGS_Tests
+{
+    [TestClass]
+    public class Controllers
+    {
+        [TestMethod]
+        public void HomeIndex()
+        {
+            //Arrange
+            HomeController hControl = new HomeController();
+            //Act
+            ViewResult result = hControl.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Login()
+        {
+            //Arrange
+            HomeController hControl = new HomeController();
+            string email = "user@test.com";
+            string password = "password";
+            //Act
+            ViewResult result = hControl.Login() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
