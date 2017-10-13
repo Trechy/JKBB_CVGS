32837504107ec48c2902aa930ba6bffceaee6422
bpaquette6801 <b.p.paquette@hotmail.com>
2017-10-13 13:31:05 -0400

    Unit Tests
    Completed/worked on some unit tests, some were ok. most were unfinished :)

--- JKBB_CVGS/Controllers/HomeController.cs
+++ JKBB_CVGS/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace JKBB_CVGS.Controllers
             {               
                 if (login.IsValid(login.Email, login.Password))
                 {
-                    return View("Index", "Home",new { login.Email});
+                    return RedirectToAction("Index", "Home",new { login.Email});
                 }
                 else
                 {
@@ -91,7 +91,7 @@ namespace JKBB_CVGS.Controllers
 
         public ActionResult LogOut()
         {
-            return View("SignUp");
+            return RedirectToAction("SignUp", "Home");
         }
 
     }
--- JKBB_CVGS_Tests/Controllers_Tests.cs
+++ JKBB_CVGS_Tests/Controllers_Tests.cs
@@ -10,12 +10,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JKBB_CVGS;
 using JKBB_CVGS.Controllers;
 using JKBB_CVGS.Models.ViewModels;
+using JKBB_CVGS.Models;
 
 namespace JKBB_CVGS_Tests
 {
     [TestClass]
     public class Controllers_Tests
     {
+        private CVGS_Context db = new CVGS_Context();
         [TestMethod]
         public void Can_Got_To_Home_Index()
         {
@@ -30,7 +32,33 @@ namespace JKBB_CVGS_Tests
         [TestMethod]
         public void SignUp()
         {
-
+            
+            //Arrange
+            HomeController hControl = new HomeController();
+            int ID = 10;
+            string UserName = "hello";
+            string Email = "new@new.com";
+            string Password = "password1";
+            string FirstName = "jason";
+            string LastName = "guy";
+            Member member = new Member();
+            SignUp signUp = new SignUp();
+            //Act
+            signUp.MemberID = ID;
+            signUp.LastName = LastName;
+            signUp.UserName = UserName;
+            signUp.Password = Password;
+            signUp.Email = Email;
+            signUp.FirstName = FirstName;
+            member.MemberID = ID;
+            member.LastName = LastName;
+            member.UserName = UserName;
+            member.Password = Password;
+            member.Email = Email;
+            member.FirstName = FirstName;
+            ViewResult result = hControl.SignUp(member, signUp) as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
         }
         [TestMethod]
         public void Can_Login()
@@ -41,14 +69,38 @@ namespace JKBB_CVGS_Tests
             loginUser.Email = "user@test.com";
             loginUser.Password = "password";
             //Act 
-            ViewResult result = hControl.Login(loginUser) as ViewResult;
+            var result = (RedirectToRouteResult)hControl.Login(loginUser);
             //Assert
-            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("Home", result.RouteValues["controller"]);
         }
         [TestMethod]
         public void Add_Game()
         {
+            //"GameID,Title,Developer,Publisher,ReleasedDate,BasePrice,Discount"
+            //Arrange
+            GameController gControl = new GameController();
+            int ID = 10;
+            string Title = "Mario+Rabbids";
+            string Developer = "Ubisoft";
+            string Publisher = "Nintendo";
+            DateTime ReleasedDate = new DateTime(2017, 10, 13); ;
+            decimal BasePrice = 10;
+            double Discount = 0.05;
+            Game game = new Game();
+            //Act
+            game.GameID = ID;
+            game.Title = Title;
+            game.Developer = Developer;
+            game.Publisher = Publisher;
+            game.ReleasedDate = ReleasedDate;
+            game.BasePrice = BasePrice;
+            game.Discount = Discount;
 
+            db.Games.Add(game);
+            var result = (RedirectToRouteResult)gControl.Create(game);
+            //Assert
+            Assert.IsNotNull(db.Games.Find(game));
         }
         [TestMethod]
         public void Edit_Game()
@@ -80,22 +132,22 @@ namespace JKBB_CVGS_Tests
         {
 
         }
-        [TestMethod]
-        public void Logout()
-        {
-            //Arrange
-            HomeController hControl = new HomeController();
-            Login loginUser = new Login();
-            loginUser.Email = "user@test.com";
-            loginUser.Password = "password";
+        //[TestMethod]
+        //public void Logout()
+        //{
+        //    //Arrange
+        //    HomeController hControl = new HomeController();
+        //    Login loginUser = new Login();
+        //    loginUser.Email = "user@test.com";
+        //    loginUser.Password = "password";
 
-            hControl.Login(loginUser);
-            //Act
-            var result = hControl.Index(loginUser.Email) as ViewResult;
-            result = result.LogOut();
-            //Assert
-            Assert.AreEqual("SignUp", result.RouteValues["action"]);
-            Assert.AreEqual("Home", result.RouteValues["controller"]);
-        }
+        //    hControl.Login(loginUser);
+        //    //Act
+        //    var result = hControl.Index(loginUser.Email) as ViewResult;
+        //    result = result.LogOut();
+        //    //Assert
+        //    Assert.AreEqual("SignUp", result.RouteValues["action"]);
+        //    Assert.AreEqual("Home", result.RouteValues["controller"]);
+        //}
     }
 }
