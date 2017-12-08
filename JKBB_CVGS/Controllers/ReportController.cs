8c65898eb3d3193386202f64bc035ac4c95f31c2
KatrinaTingTingLin <31769438+KatrinaTingTingLin@users.noreply.github.com>
2017-12-08 13:17:23 -0500

    Modified report

--- JKBB_CVGS/Controllers/ReportController.cs
+++ JKBB_CVGS/Controllers/ReportController.cs
@@ -12,57 +12,22 @@ namespace JKBB_CVGS.Controllers
 {
     public class ReportController : Controller
     {
+        private CVGS_Context db = new CVGS_Context();
         // GET: Report
         [CustomAuthorize(Roles = "Employee")]
         public ActionResult Index()
         {
-            //Query for member list report
-            using (var context = new CVGS_Context())
-            {
-                var L2EQuery = from m in context.Users 
-                               select new {
-                                   UserName =  m.FirstName + m.LastName,
-                                   UserEmail = m.Email,
-                                   ItemPurchased = "0"
-                               };
-                var userNameList = L2EQuery.ToDictionary(m => "UserName", m => m.UserName);
-                var userEmailList = L2EQuery.ToDictionary(m => "UserEmail", m => m.UserEmail);
-                var itemPurchased = L2EQuery.ToDictionary(m => "ItemPurchased", m => m.ItemPurchased);
-
-                ViewBag.UserNameList = userNameList;
-                ViewBag.UserEmailList = userEmailList;
-                ViewBag.ItemPurchasedList = itemPurchased;
-            }
+            //Query for user list report
+            List<User> users = db.Users.ToList();
+            ViewBag.users = users;
 
             //Query for game list report 
-            using (var context = new CVGS_Context())
-            {
+            List<Game> games = db.Games.ToList();
+            ViewBag.games = games;
 
-
-                var L2EQuery = from g in context.Games
-                               select new
-                               {
-                                   Title = g.Title,
-                                   Developer = g.Developer,
-                                   ReleasedDate = g.ReleasedDate,
-                                   UserRating = "0",
-                                   BasePrice = g.BasePrice,
-                                   Discount = g.Discount
-                               };
-                var gameTitleList = L2EQuery.ToDictionary(m => "Title", m => m.Title);
-                var developerList = L2EQuery.ToDictionary(m => "Developer", m => m.Developer);
-                var releaseDateList = L2EQuery.ToDictionary(m => "ReleasedDate", m => m.ReleasedDate);
-                var userRatingList = L2EQuery.ToDictionary(m => "UserRating", m => m.UserRating);
-                var basePriceList = L2EQuery.ToDictionary(m => "BasePrice", m => m.BasePrice);
-                var discountList = L2EQuery.ToDictionary(m => "Discount", m => m.Discount);
-
-                ViewBag.GameTitleList = gameTitleList;
-                ViewBag.DeveloperList = developerList;
-                ViewBag.ReleasedDateList = releaseDateList;
-                ViewBag.UserRatingList = userRatingList;
-                ViewBag.BasePriceList = basePriceList;
-                ViewBag.DiscountList = discountList;
-            }
+            //Query for wishlist report 
+            List<Wishlist> wishlists = db.Wishlists.ToList();
+            ViewBag.wishlists = wishlists;
 
             return View();
         }
